5473ade1eda6b07b206d5ddd27c78e92ce941fcb
Rod Morgado <rodrigo@zooft.company>
2022-04-10 12:46:06 +0200

    Well done! You can see that working with a single chunk in a nested data frame is identical to working with regular data frames. In the next section you will learn how to scale this approach to work on a vcetor of nested data frames using the map family function.

--- supervised-machine-learning.R
+++ supervised-machine-learning.R
@@ -9,3 +9,22 @@ gap_nested <- gapminder %>%
 
 # Explore gap_nested
 head(gap_nested, 6)
+
+# Create the unnested data frame called gap_unnnested
+gap_unnested <- gap_nested %>% 
+  unnest()
+  
+# Confirm that your data was not modified  
+identical(gapminder, gap_unnested)
+
+# Extract the data of Algeria
+algeria_df <- gap_nested$data[[1]]
+
+# Calculate the minimum of the population vector
+min(algeria_df$population)
+
+# Calculate the maximum of the population vector
+max(algeria_df$population)
+
+# Calculate the mean of the population vector
+mean(algeria_df$population)
