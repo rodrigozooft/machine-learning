7fbb291927427869fd5abcbc3d00a9d179b7dceb
Rod Morgado <rodrigo@zooft.company>
2022-04-21 17:26:31 -0700

    Using training() and testing() after the split ensures that youo save your working datasets. Sharpen your sword for some more splitting magic.

--- decision-trees.R
+++ decision-trees.R
@@ -23,3 +23,13 @@ tree_model_bmi <- tree_spec %>%
 # Print the model
 tree_model_bmi
 
+# Create the split
+diabetes_split <- initial_split(diabetes, prop = 0.8)
+
+# Extract the training and test set
+diabetes_train <- training(diabetes_split)
+diabetes_test  <- testing(diabetes_split)
+
+# Verify the proportions of both sets
+round(nrow(diabetes_train) / nrow(diabetes), 2) == 0.80
+round(nrow(diabetes_test) / nrow(diabetes), 2) == 0.20
