52c971cacb7e830f10a8b3de0c1931d5141e7a39
Rod Morgado <rodrigo@zooft.company>
2022-04-28 14:11:28 -0700

    You are now ready for the next lesson in which we'll visually explore the model.

--- support-vector-machine.R
+++ support-vector-machine.R
@@ -68,3 +68,28 @@ trainColNum <- grep("train", names(df))
 #remove "train" column from train and test dataset
 trainset <- trainset[, -trainColNum]
 testset <- testset[, -trainColNum]
+
+library(e1071)
+
+#build svm model, setting required parameters
+svm_model<- svm(y ~ ., 
+                data = trainset, 
+                type = "C-classification", 
+                kernel = "linear", 
+                scale = FALSE)
+
+#list components of model
+names(svm_model)
+
+#list values of the SV, index and rho
+svm_model$SV
+svm_model$index
+svm_model$rho
+
+#compute training accuracy
+pred_train <- predict(svm_model, trainset)
+mean(pred_train == trainset$y)
+
+#compute test accuracy
+pred_test <- predict(svm_model, testset)
+mean(pred_test == testset$y)
