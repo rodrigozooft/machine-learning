c2521d0eab546f29f0bb217905423487bd2d189d
Rod Morgado <rodrigo@zooft.company>
2022-04-29 15:55:45 -0700

    We'll see how to build a better SVM for radially separable data.

--- support-vector-machine.R
+++ support-vector-machine.R
@@ -244,3 +244,24 @@ pred_train <- predict(svm_model_2, trainset)
 mean(pred_train == trainset$y)
 pred_test <- predict(svm_model_2, testset)
 mean(pred_test == testset$y)
+
+# Print average accuracy and standard deviation
+accuracy <- rep(NA, 100)
+set.seed(2)
+
+# Calculate accuracies for 100 training/test partitions
+for (i in 1:100){
+    df[, "train"] <- ifelse(runif(nrow(df)) < 0.8, 1, 0)
+    trainset <- df[df$train == 1, ]
+    testset <- df[df$train == 0, ]
+    trainColNum <- grep("train", names(trainset))
+    trainset <- trainset[, -trainColNum]
+    testset <- testset[, -trainColNum]
+    svm_model <- svm(y ~ ., data = trainset, type = "C-classification", kernel = "linear")
+    pred_test <- predict(svm_model, testset)
+    accuracy[i] <- mean(pred_test == testset$y)
+}
+
+# Print average accuracy and standard deviation
+mean(accuracy)
+sd(accuracy)
