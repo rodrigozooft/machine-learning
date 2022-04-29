73d443e918f9d7063c2bdbecf4fefbca91226c3c
Rod Morgado <rodrigo@zooft.company>
2022-04-29 16:26:06 -0700

    The boundary is ready!

--- support-vector-machine.R
+++ support-vector-machine.R
@@ -318,3 +318,24 @@ mean(pred_test == testset$y)
 
 #plot model
 plot(svm_model, trainset)
+
+#number of data points
+n <- 1000
+
+#set seed
+set.seed(1)
+
+#create dataframe
+df <- data.frame(x1 = rnorm(n, mean = -0.5, sd = 1), 
+                 x2 = runif(n, min = -1, max = 1))
+
+#set radius and centers
+radius <- 0.8
+center_1 <- c(-0.8, 0)
+center_2 <- c(0.8, 0)
+radius_squared <- radius^2
+
+#create binary classification variable
+df$y <- factor(ifelse((df$x1-center_1[1])^2 + (df$x2-center_1[2])^2 < radius_squared|
+                      (df$x1-center_2[1])^2 + (df$x2-center_2[2])^2 < radius_squared, -1, 1),
+                      levels = c(-1, 1))
