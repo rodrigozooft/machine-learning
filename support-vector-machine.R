962a002084f2c6284b62e73923486290374c3339
Rod Morgado <rodrigo@zooft.company>
2022-04-29 16:42:22 -0700

    The model accuracy is not too bad, but the polot shows that it is impossible to capture the figure of 8 shape of the actual boundary using a degree 2 polynomial.

--- support-vector-machine.R
+++ support-vector-machine.R
@@ -366,3 +366,17 @@ mean(pred_test == testset$y)
 
 #plot model against testset
 plot(svm_model, testset)
+
+#build model
+svm_model<- 
+    svm(y ~ ., data = trainset, type = "C-classification", 
+        kernel = "polynomial", degree = 2)
+
+#accuracy
+pred_train <- predict(svm_model, trainset)
+mean(pred_train == trainset$y)
+pred_test <- predict(svm_model, testset)
+mean(pred_test == testset$y)
+
+#plot model
+plot(svm_model, trainset)
