7dd50e707ef87e1469f8395c1aa7b4e2307455d3
Rod Morgado <rodrigo@zooft.company>
2022-04-29 16:04:21 -0700

    As expected, the data is linearly separable in the x1^2 - x2^2 plane.

--- support-vector-machine.R
+++ support-vector-machine.R
@@ -265,3 +265,15 @@ for (i in 1:100){
 # Print average accuracy and standard deviation
 mean(accuracy)
 sd(accuracy)
+
+#transform data
+df1 <- data.frame(x1sq = df$x1^2, x2sq = df$x2^2, y = df$y)
+
+#plot data points in the transformed space
+plot_transformed <- ggplot(data = df1, aes(x = x1sq, y = x2sq, color = y)) + 
+    geom_point()+ guides(color = FALSE) + 
+    scale_color_manual(values = c("red", "blue"))
+
+#add decision boundary and visualize
+plot_decision <- plot_transformed + geom_abline(slope = -1, intercept = 0.64)
+plot_decision
