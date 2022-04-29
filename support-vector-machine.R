67cac29fb62ca79a40ce5723ff3051d40c0df84a
Rod Morgado <rodrigo@zooft.company>
2022-04-29 15:30:23 -0700

    Creating the circle boundary

--- support-vector-machine.R
+++ support-vector-machine.R
@@ -197,3 +197,19 @@ for (i in 1:100){
 }
 mean(accuracy) 
 sd(accuracy)
+
+#set number of variables and seed
+n <- 400
+set.seed(1)
+
+#Generate data frame with two uniformly distributed predictors, x1 and x2
+df <- data.frame(x1 = runif(n, min = -1, max = 1), 
+                 x2 = runif(n, min = -1, max = 1))
+
+#We want a circular boundary. Set boundary radius 
+radius <- 0.8
+radius_squared <- radius^2
+
+#create dependent categorical variable, y, with value -1 or 1 depending on whether point lies
+#within or outside the circle.
+df$y <- factor(ifelse(df$x1^2 + df$x2^2 < radius_squared, -1, 1), levels = c(-1, 1))
