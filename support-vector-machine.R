b9071d599b662408503084f8bb58ddfc256214d7
Rod Morgado <rodrigo@zooft.company>
2022-04-29 17:09:32 -0700

    Note that the average accuracy is almost 10% better than the one obtained in the previous exercise.

--- support-vector-machine.R
+++ support-vector-machine.R
@@ -401,3 +401,24 @@ for (i in 1:100){
 #print average accuracy and standard deviation
 mean(accuracy)
 sd(accuracy)
+
+#create vector to store accuracies and set random number seed
+accuracy <- rep(NA, 100)
+set.seed(2)
+
+#calculate accuracies for 100 training/test partitions
+for (i in 1:100){
+    df[, "train"] <- ifelse(runif(nrow(df))<0.8, 1, 0)
+    trainset <- df[df$train == 1, ]
+    testset <- df[df$train == 0, ]
+    trainColNum <- grep("train", names(trainset))
+    trainset <- trainset[, -trainColNum]
+    testset <- testset[, -trainColNum]
+    svm_model<- svm(y ~ ., data = trainset, type = "C-classification", kernel = "radial")
+    pred_test <- predict(svm_model, testset)
+    accuracy[i] <- mean(pred_test == testset$y)
+}
+
+#print average accuracy and standard deviation
+mean(accuracy)
+sd(accuracy)
