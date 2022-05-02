ae5b97c9b399a5a99e680610451274ebc1725a26
Rod Morgado <rodrigo@zooft.company>
2022-05-02 16:54:02 -0700

    Note that there are slightly more predicted mines with this lower threshold: 58 (40 + 18) as compared to 47 for the 0.5 threshold.

--- machine-learning-fundamentals.R
+++ machine-learning-fundamentals.R
@@ -128,3 +128,12 @@ p_class <- factor(m_or_r, levels = levels(test[["Class"]]))
 
 # Create confusion matrix
 confusionMatrix(p_class, test[["Class"]])
+
+# If p exceeds threshold of 0.1, M else R: m_or_r
+m_or_r <- ifelse(p > 0.1, "M", "R")
+
+# Convert to factor: p_class
+p_class <- factor(m_or_r, levels = levels(test[["Class"]]))
+
+# Create confusion matrix
+confusionMatrix(p_class, test[["Class"]])
