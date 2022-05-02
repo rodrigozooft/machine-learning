9120ea29173ffeb46927cb38863e5eb0a4eb22b0
Rod Morgado <rodrigo@zooft.company>
2022-05-02 12:08:26 -0700

    Because you already randomly ordered your dataset, it's easy to split off a random test set.

--- machine-learning-fundamentals.R
+++ machine-learning-fundamentals.R
@@ -20,3 +20,12 @@ rows <- sample(nrow(diamonds))
 
 # Randomly order data
 shuffled_diamonds <- diamonds[rows, ]
+
+# Determine row to split on: split
+split <- round(nrow(diamonds) * 0.8)
+
+# Create train
+train <- diamonds[1:split, ]
+
+# Create test
+test <- diamonds[(split + 1):nrow(diamonds), ] 
