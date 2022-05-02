1cf8e91035e0ec30fa92f58446961eac2e4480b6
Rod Morgado <rodrigo@zooft.company>
2022-05-02 16:52:00 -0700

    Note that there are (sligthly) fewer predicted mines this higher threshold: 55 (40 + 15) as compared to 57 for the 0.50 threshold.

--- machine-learning-fundamentals.R
+++ machine-learning-fundamentals.R
@@ -119,3 +119,12 @@ p_class <- factor(m_or_r, levels = levels(test[["Class"]]))
 
 # Create confusion matrix
 confusionMatrix(p_class, test[["Class"]])
+
+# If p exceeds threshold of 0.9, M else R: m_or_r
+m_or_r <- ifelse(p > 0.9, "M", "R")
+
+# Convert to factor: p_class
+p_class <- factor(m_or_r, levels = levels(test[["Class"]]))
+
+# Create confusion matrix
+confusionMatrix(p_class, test[["Class"]])
