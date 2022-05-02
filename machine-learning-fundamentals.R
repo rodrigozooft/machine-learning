1fe45c86e8fddd1ab04d78befffc7ddef620344d
Rod Morgado <rodrigo@zooft.company>
2022-05-02 12:28:54 -0700

    Randomly shuffling your dataa makees it easy to manually create a train/test split.

--- machine-learning-fundamentals.R
+++ machine-learning-fundamentals.R
@@ -84,3 +84,23 @@ model <- train(
 )
 
 # Print model to console
+# Predict on full Boston dataset
+predict(model, Boston)
+
+# Get the number of observations
+n_obs <- nrow(Sonar)
+
+# Shuffle row indices: permuted_rows
+permuted_rows <- sample(n_obs)
+
+# Randomly order data: Sonar
+Sonar_shuffled <- Sonar[permuted_rows, ]
+
+# Identify row to split on: split
+split <- round(n_obs * 0.6)
+
+# Create train
+train <- Sonar_shuffled[1:split, ]
+
+# Create test
+test <- Sonar_shuffled[(split + 1):nrow(Sonar_shuffled), ]
