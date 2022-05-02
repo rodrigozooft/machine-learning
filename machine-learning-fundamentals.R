28094416d2d750e213f7b42e43381c2494996eb2
Rod Morgado <rodrigo@zooft.company>
2022-05-02 12:22:21 -0700

    You can use caret to do some very complicated cross-validation schemes.

--- machine-learning-fundamentals.R
+++ machine-learning-fundamentals.R
@@ -69,3 +69,18 @@ model <- train(
 )
 
 # Print model to console
+
+# Fit lm model using 5 x 5-fold CV: model
+model <- train(
+  medv ~ ., 
+  Boston,
+  method = "lm",
+  trControl = trainControl(
+    method = "repeatedcv", 
+    number = 5,
+    repeats = 5, 
+    verboseIter = TRUE
+  )
+)
+
+# Print model to console
