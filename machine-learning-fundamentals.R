f2376313d97a65da66119a7681b43e6922497394
Rod Morgado <rodrigo@zooft.company>
2022-05-02 15:37:20 -0700

    The confusionMatrix function is a very easy way to get a detalied summary of your model's accuracy.

--- machine-learning-fundamentals.R
+++ machine-learning-fundamentals.R
@@ -104,3 +104,18 @@ train <- Sonar_shuffled[1:split, ]
 
 # Create test
 test <- Sonar_shuffled[(split + 1):nrow(Sonar_shuffled), ]
+
+# Fit glm model: model
+model <- glm(Class ~ ., data = train, family = "binomial")
+
+# Predict on test: p
+p <- predict(model, test, type = "response")
+
+# If p exceeds threshold of 0.5, M else R: m_or_r
+m_or_r <- ifelse(p > 0.5, "M", "R")
+
+# Convert to factor: p_class
+p_class <- factor(m_or_r, levels = levels(test[["Class"]]))
+
+# Create confusion matrix
+confusionMatrix(p_class, test[["Class"]])
